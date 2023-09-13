380dceb1540f63c344f40f3a346c968a423d1f23
nadaelmahfoudi <144923941+nadaelmahfoudi@users.noreply.github.com>
2023-09-14 00:33:13 +0200

    Create challenge7.c

---
+++ challenge7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main()
+
+{
+     int u,d, c, inv;
+    
+    printf("veuillez entrez un nombre entier %d %d %d \n", u, d, c);
+    scanf("%d %d %d",&u,&d,&c);
+    printf("%d %d %d",c,d,u);
+    return 0;
+}
